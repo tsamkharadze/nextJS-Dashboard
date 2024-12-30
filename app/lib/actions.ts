a77101b3e0657c082d70a9717dceb8fc741c4192
tsamkharadze <samkaradze@gmail.com>
2024-12-30 18:56:02 +0400

    delete vercel errors 1.2

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -108,14 +108,13 @@ export async function updateInvoice(
   redirect("/dashboard/invoices");
 }
 
-export async function deleteInvoice(id: string) {
+export async function deleteInvoice(id: string): Promise<void> {
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath("/dashboard/invoices");
-    return { message: "Deleted Invoice" };
   } catch (error) {
-    console.error(error); // Log error
-    return { message: "Database Error: Failed to Delete Invoice" };
+    console.error("Failed to delete invoice:", error); // Log error
+    throw new Error("Database Error: Failed to Delete Invoice");
   }
 }
 
